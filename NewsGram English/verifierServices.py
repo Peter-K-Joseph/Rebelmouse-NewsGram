8c09e2f79855abc0b78237ee93ecca1fc1f4b6e4
Peter K Joseph <69970683+Peter-K-Joseph@users.noreply.github.com>
2021-07-28 09:26:05 +0530

    Update verifierServices.py

--- NewsGram English/verifierServices.py
+++ NewsGram English/verifierServices.py
@@ -9,7 +9,7 @@ import html
 
 # Configuration
 header = {
-    'X-RMAuth': 'KlCym4YdO1BQrvAWjNkuHBPhaw5OxkS4wENNlQbaKCEhqOpxyewBWVzV4479IpDm', 
+    'X-RMAuth': '${XAUTH_KEY_FOR_NEWSGRAM_ENGLISH}', 
     'User-Agent': 'PostmanRuntime/7.28.0', 
     'Accept': '*/*', 
     'Cache-Control': 'no-cache', 
